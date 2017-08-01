e5e6d27296fe02f6f3f933766d97c66467cf6c8f
tristen <tristen.brown@gmail.com>
2017-08-01 18:22:33 -0400

    Basic example

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,13 +1,50 @@
-'use strict';
-
 import React from 'react'
 import ReactDOM from 'react-dom'
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
 class Application extends React.Component {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      lng: 5,
+      lat: 34,
+      zoom: 1.5
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.refs.map,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom: 1.5
+    });
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+
   render() {
+    const { lng, lat, zoom } = this.state;
+
     return (
       <div>
-        Hello world
+        <div className='inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold'>
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+        </div>
+        <div className='absolute top right left bottom' ref='map' />
       </div>
     );
   }
