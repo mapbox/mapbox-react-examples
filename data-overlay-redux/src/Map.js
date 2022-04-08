ac276eee8c1ac3bb1b189fbc5e110250a55099a0
Josh Kautz <josh@joshkautz.com>
2022-04-08 18:04:13 -0500

    Redacted access token

--- data-overlay-redux/src/Map.js
+++ data-overlay-redux/src/Map.js
@@ -25,7 +25,7 @@ function mapStateToPropsOptionsfield(state) {
 }
 
 mapboxgl.accessToken =
-  'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+  Redacted access token
 
 const Map = props => {
   const mapContainerRef = useRef(null);
