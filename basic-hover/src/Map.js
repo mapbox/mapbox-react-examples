83954bedce9a7f996ef30903addbbd0107003958
yewyewXD <yewyew6933@gmail.com>
2023-09-11 12:50:35 +0700

    add country hover & click effect on map

--- basic-hover/src/Map.js
+++ basic-hover/src/Map.js
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import './Map.css';
+import data from './data.json';
 
 mapboxgl.accessToken = 'your-api-key';
 
@@ -22,6 +23,87 @@ const Map = () => {
 
     // Add navigation control (the +/- zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+    map.on('load', () => {
+      map.addSource('countries', {
+        type: 'geojson',
+        data,
+      });
+
+      // Add layer on every country
+      map.addLayer({
+        id: 'country-fills',
+        type: 'fill',
+        source: 'countries',
+        layout: {},
+        paint: {
+          'fill-color': '#fff',
+          'fill-opacity': 0,
+        },
+      });
+
+      // Add country borders
+      map.addLayer({
+        id: 'country-borders',
+        type: 'line',
+        source: 'countries',
+        layout: {},
+        paint: {
+          'line-color': '#627BC1',
+          'line-width': 2,
+        },
+      });
+
+      // Add darken layer on hover over country
+      map.addLayer({
+        id: 'country-fills-hover',
+        type: 'fill',
+        source: 'countries',
+        layout: {},
+        paint: {
+          'fill-color': '#000000',
+          'fill-opacity': 0.3,
+        },
+        filter: ['==', 'name', ''],
+      });
+
+      // Add mouse hover listener
+      map.on('mousemove', (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ['country-fills'],
+        });
+
+        if (features.length) {
+          map.getCanvas().style.cursor = 'pointer';
+          map.setFilter('country-fills-hover', [
+            '==',
+            'name',
+            features[0].properties.name,
+          ]);
+        } else {
+          map.setFilter('country-fills-hover', ['==', 'name', '']);
+          map.getCanvas().style.cursor = '';
+        }
+      });
+
+      // Add mouse un-hover listener
+      map.on('mouseout', () => {
+        map.getCanvas().style.cursor = 'auto';
+        map.setFilter('country-fills-hover', ['==', 'name', '']);
+      });
+
+      // Add country onclick effect
+      map.on('click', (e) => {
+        const features = map.queryRenderedFeatures(e.point, {
+          layers: ['country-fills'],
+        });
+        if (!features.length) return;
+        const {
+          properties: { name, iso },
+        } = features[0];
+        alert(`You clicked on ${name} (${iso})!`);
+      });
+    });
 
     map.on('move', () => {
       setLng(map.getCenter().lng.toFixed(4));
